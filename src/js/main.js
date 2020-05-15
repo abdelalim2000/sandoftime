31990f38b56a1963b8f6ddd9700ff16834315545
Mohamed <mohamed.abdelalilm2000@gmail.com>
2020-05-15 20:09:27 +0200

    New Version of the code working

--- src/js/main.js
+++ src/js/main.js
@@ -1,47 +1,45 @@
-var carousel = document.getElementById("main");
-var change1 = document.getElementById("changed");
-var change2 = document.getElementById("changed-two");
+var imageSlider = document.getElementsByClassName("slide");
 var prev = document.getElementById("prev");
 var next = document.getElementById("next");
+var current = 0;
 
-var myBG = [
-  "url(src/img/beach-bridge-dark-clouds-dawn.jpg)",
-  "url(src/img/action-adventure-aerial-aerial-shot-312105.jpg)",
-  "url(src/img/beach-water-steps-sand-17727.jpg)",
-  "url(src/img/clouds-dawn-nature-ocean-561463.jpg)",
-];
-
-var i = 1;
-function changeCarousel() {
-  carousel.style.background = `linear-gradient(to right, rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)),${myBG[i]} no-repeat`;
-  carousel.style.backgroundAttachment = "fixed";
-  carousel.style.backgroundSize = "cover";
-  carousel.style.backgroundPosition = "center center";
+function reset() {
+  for (var i = 0; i < imageSlider.length; i++) {
+    imageSlider[i].style.display = "none";
+  }
 }
 
-function changeText() {
-  change1.style.background = `${myBG[i]} no-repeat`;
-  change1.style.webkitBackgroundClip = "text";
-  change1.style.webkitTextFillColor = "transparent";
-  change1.style.backgroundAttachment = "fixed";
-  change1.style.backgroundSize = "cover";
-  change1.style.backgroundPosition = "center center";
+function startSlide() {
+  reset();
+  imageSlider[0].style.display = "block";
 }
-function changeText_two() {
-  change2.style.background = `${myBG[i]} no-repeat`;
-  change2.style.webkitBackgroundClip = "text";
-  change2.style.webkitTextFillColor = "transparent";
-  change2.style.backgroundAttachment = "fixed";
-  change2.style.backgroundSize = "cover";
-  change2.style.backgroundPosition = "center center";
+
+function prevImage() {
+  reset();
+  imageSlider[current - 1].style.display = "block";
+  current--;
+}
+
+prev.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (current === 0) {
+    current = imageSlider.length;
+  }
+  prevImage();
+});
+
+function nextImage() {
+  reset();
+  imageSlider[current + 1].style.display = "block";
+  current++;
 }
 
 next.addEventListener("click", function (e) {
   e.preventDefault();
-
-  changeCarousel();
-  changeText_two();
-  changeText();
-
-  i = (i + 1) % myBG.length;
+  if (current === imageSlider.length - 1) {
+    current = -1;
+  }
+  nextImage();
 });
+
+startSlide();
